028acec39085ed60ca8d2c15132c2224a3fe62c0
Mariuspageot <mariuspageot@gmail.com>
2021-02-02 15:17:14 +0100

    troisième build

---
+++ docs/precache-manifest.3ef19787998fb586b5675938affe59ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1f38cdf8103aa69590",
+    "url": "/vue-app-2/css/app.031024d8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3f80f20dffaddddbb031c57fd33b41f4",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "64554453eab5c8d78125",
+    "url": "/vue-app-2/js/about.89c8a07e.js"
+  },
+  {
+    "revision": "1d1f38cdf8103aa69590",
+    "url": "/vue-app-2/js/app.0cada8a2.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
